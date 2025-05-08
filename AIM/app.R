240820cf195335a5105ca741c1922ea1a0046933
David Allen <dallen@merceradvisors.com>
2025-05-07 17:09:21 -0700

    Adds strategy type and tax-managed filters

--- AIM/app.R
+++ AIM/app.R
@@ -1,50 +1,87 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    https://shiny.posit.co/
-#
+
 
 library(shiny)
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
-
-    # Application title
-    titlePanel("Old Faithful Geyser Data"),
-
-    # Sidebar with a slider input for number of bins 
-    sidebarLayout(
-        sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
-        ),
-
-        # Show a plot of the generated distribution
-        mainPanel(
-           plotOutput("distPlot")
-        )
+  
+  # Application title
+  titlePanel("Aspen Investing Menu"),
+  
+  # Sidebar with a slider input for number of bins 
+  sidebarLayout(
+    sidebarPanel(
+      checkboxGroupInput(
+        
+        inputId = "strategy_types", 
+        
+        label   = "Type", 
+        
+        choiceNames = c(
+          "Market", 
+          "Multifactor", 
+          "Income", 
+          "Equity", 
+          "Fixed Income"),
+        
+        choiceValues = c(
+          "Market Series", 
+          "Multifactor Series", 
+          "Income Series", 
+          "Equity Strategies", 
+          "Fixed Income Strategies"),
+        
+        selected = c(
+          "Market Series", 
+          "Multifactor Series", 
+          "Income Series", 
+          "Equity Strategies", 
+          "Fixed Income Strategies"),
+        
+        inline = TRUE),
+      
+      checkboxGroupInput(
+        inputId = "tm_status",
+        label   = "Tax-Managed",
+        choiceNames = c("Yes", "No"),
+        choiceValues = c(TRUE, FALSE),
+        selected = c(TRUE, FALSE),
+        inline = TRUE)
+      
+      
+    ),
+    
+    # Show a plot of the generated distribution
+    mainPanel(
+      DT::DTOutput("strategies")
     )
+  )
 )
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
-
-    output$distPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white',
-             xlab = 'Waiting time to next eruption (in mins)',
-             main = 'Histogram of waiting times')
-    })
+  
+  platform <- aws.s3::get_object(
+    region = Sys.getenv("AWS_DEFAULT_REGION"),
+    key    = Sys.getenv("AWS_ACCESS_KEY_ID"),
+    secret = Sys.getenv("AWS_SECRET_ACCESS_KEY"),
+    
+    object = "orion-platform.csv", 
+    bucket = "aspen-investing-menu") |>
+    readBin("character") |>
+    readr::read_csv(show_col_types = FALSE) |>
+    dplyr::select(strategy, type, model, portfolio, tax_managed) |>
+    dplyr::distinct()
+  
+  dat <- reactive({
+    platform |>
+      dplyr::filter(type %in% input$strategy_types) 
+  })
+  
+  output$strategies <- DT::renderDataTable(
+    DT::datatable(dat(), options = list(paging = FALSE))
+    
+  )
 }
 
 # Run the application 
